0b2c8c12947665083070977ae66c98854fc0bdc3
Geniards <geniard93@gmail.com>
2025-02-21 03:50:15 +0900

    [Feat] 그리드매니저 싱글턴 추가

--- Assets/Scripts/01_Manager/GridManager.cs
+++ Assets/Scripts/01_Manager/GridManager.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    public static GridManager Instance { get; private set; }
+
     public GameObject tilePrefab;   // Tile 프리펩
-    public TextAsset jsonFile;      // JSON 파일
 
     // 해당 좌표의 값에 따른 타일 검색
     public Dictionary<Vector2Int, Tile> tileDictionary = new Dictionary<Vector2Int, Tile>();
 
+    private void Awake()
+    {
+        if(!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     /// <summary>
     /// Json기반의 데이터로 그리드 생성.
     /// </summary>
-    public void LoadGridFromJson()
+    public void LoadGrid(StageData stageData)
     {
-        // Json 파싱
-        StageData stageData = JsonUtility.FromJson<StageData>(jsonFile.text);
-
+        ResetGrid();
         // 그리드 생성
         GenerateGridFromTileMap(stageData);
     }
@@ -34,11 +45,12 @@ public class GridManager : MonoBehaviour
                 if(stageData.tileLegend.TryGetValue(tileCode.ToString(), out TileInfo tileInfo))
                 {
                     Vector2Int position = new Vector2Int(x, stageData.height - y - 1);
-                    GameObject tileObj = Instantiate(tilePrefab, new Vector3(x,-y,0), Quaternion.identity);
-                    Tile tile = tileObj.GetComponent<Tile>();
 
+                    // 오브젝트 풀에서 타일 가져오기
+                    Tile tile = GameManager.Instance.GetTile();
                     tile.SetTilePosition(position);
                     tile.SetTileProperties(tileInfo.walkable, Enum.Parse<E_TileType>(tileInfo.type));
+
                     tileDictionary[position] = tile;
                 }
             }
@@ -56,4 +68,15 @@ public class GridManager : MonoBehaviour
         return tile;
     }
 
+    /// <summary>
+    /// 스테이지 변경 시 기존 타일 초기화(오브젝트 풀로 반환)
+    /// </summary>
+    public void ResetGrid()
+    {
+        foreach(var tile in tileDictionary.Values)
+        {
+            GameManager.Instance.ReturnTile(tile);
+        }
+        tileDictionary.Clear();
+    }
 }
