70f4bba9c5c456ac98ea7eac8e9c71ee39fcedbc
Geniards <geniard93@gmail.com>
2025-02-21 03:53:13 +0900

    [Fix] Json파일의 Dictionary의 직렬화 불가로 인한 리스트로 구조 변경

--- Assets/Scripts/99_Utilities/StageData.cs
+++ Assets/Scripts/99_Utilities/StageData.cs
@@ -10,6 +10,28 @@ public class StageData
     public string stageName;
     public int width;
     public int height;
-    public Dictionary<string, TileInfo> tileLegend;
+    public List<TileLegendEntry> tileLegendList;
     public string[] tileMap;
+
+    // 유닛 배치 정보
+    public List<UnitSpawnData> units;
+
+    public Dictionary<string, TileInfo> tileLegend;
+
+    public void ConvertTileLegend()
+    {
+        tileLegend = new Dictionary<string, TileInfo>();
+        foreach (var entry in tileLegendList)
+        {
+            tileLegend[entry.key] = entry.value;
+        }
+    }
+
+}
+
+[System.Serializable]
+public class TileLegendEntry
+{
+    public string key;
+    public TileInfo value;
 }
